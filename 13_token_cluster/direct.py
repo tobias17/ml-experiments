aab5c210e8a4cdd8f87109b0191e7174d0b29877
Tobias Fischer <tobiasfischer17@gmail.com>
2024-10-13 18:17:39 -0500

    more slimming

--- 13_token_cluster/direct.py
+++ 13_token_cluster/direct.py
@@ -1,11 +1,9 @@
-from tinygrad import Tensor, nn, Variable, dtypes, TinyJit, Device
+from tinygrad import Tensor, nn, dtypes, Device
 from tinygrad.nn.state import get_parameters
 from tinygrad.helpers import prod, BEAM, Context
-from extra.models.llama import TransformerBlock, Attention, precompute_freqs_cis, apply_rotary_emb, repeat_kv # type: ignore
 
 from typing import List, Dict, Union, Optional, Tuple
-import datetime, os, time
-import matplotlib.pyplot as plt
+import time
 import numpy as np
 
 TOKEN_DIMS   = 512
@@ -13,6 +11,8 @@ CLUSTER_SIZE = 4
 CLUSTER_DIMS = 2048
 
 MAX_CLUSTER_CONTEXT = 32
+
+
 
 class Encoder:
    def __init__(self, vocab_size:int, max_context:int, n_layers:int, token_dim:int, cluster_dim:int, cluster_size:int, d_head:int, ff_mult:float=4.0, rope_theta:int=10000):
@@ -39,8 +39,8 @@ class Decoder:
 
 
 def main():
-   BEAM_VALUE = BEAM.value
-   BEAM.value = 0
+   Tensor.manual_seed(42)
+   Tensor.training = True
 
    # Define Models
    VOCAB_SIZE = 32000
@@ -74,7 +74,7 @@ def main():
    print("")
 
    # Define the Optimizer
-   LEARNING_RATE = 2e-4
+   LEARNING_RATE = 2e-1
    optim = nn.optim.SGD(params, LEARNING_RATE)
 
    # Define some Globals
@@ -84,30 +84,28 @@ def main():
 
    step_i = 0
    dataset_i = 0
-   with Tensor.train():
-      while True:
-         start_time = time.time()
-         Tensor.manual_seed(step_i)
+   while True:
+      start_time = time.time()
+      Tensor.manual_seed(step_i)
 
-         with Context(BEAM=BEAM_VALUE):
-            orig_batches = [Tensor(np.asarray(X_train[dataset_i + batch_i*TOKENS_CONTEXT_SIZE :dataset_i + (batch_i+1)*TOKENS_CONTEXT_SIZE]), dtype=dtypes.int32) for batch_i in range(GLOBAL_BS)]
-            orig_tokens = Tensor.stack(*orig_batches)
+      orig_batches = [Tensor(np.asarray(X_train[dataset_i + batch_i*TOKENS_CONTEXT_SIZE :dataset_i + (batch_i+1)*TOKENS_CONTEXT_SIZE]), dtype=dtypes.int32) for batch_i in range(GLOBAL_BS)]
+      orig_tokens = Tensor.stack(*orig_batches)
 
-            enc_clusters = enc(orig_tokens).realize()
-            dec_tokens   = dec(enc_clusters).realize()
+      enc_clusters = enc(orig_tokens).realize()
+      dec_tokens   = dec(enc_clusters).realize()
 
-            loss = dec_tokens.sparse_categorical_crossentropy(orig_tokens).realize()
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
+      loss = dec_tokens.sparse_categorical_crossentropy(orig_tokens).realize()
+      optim.zero_grad()
+      loss.backward()
+      optim.step()
 
-            acc = (dec_tokens.argmax(axis=-1) == orig_tokens).mean().realize()
+      acc = (dec_tokens.argmax(axis=-1) == orig_tokens).mean().realize()
 
-            delta_time = time.time() - start_time
-            print(f"| {step_i:05d} | {dataset_i:08d} | {1000.0*delta_time:.0f} ms | {loss.item():.4f} Train Loss | {100.0*acc.item():.2f}% Train Acc |")
+      delta_time = time.time() - start_time
+      print(f"| {step_i:05d} | {dataset_i:08d} | {1000.0*delta_time:.0f} ms | {loss.item():.4f} Train Loss | {100.0*acc.item():.2f}% Train Acc |")
 
-         step_i += 1
-         dataset_i += TOKENS_CONTEXT_SIZE * GLOBAL_BS
+      step_i += 1
+      dataset_i += TOKENS_CONTEXT_SIZE * GLOBAL_BS
 
 if __name__ == "__main__":
    main()
