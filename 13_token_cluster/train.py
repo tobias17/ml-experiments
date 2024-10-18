a2ed6d45f60d2fc24ebae463f6f6e3eaac649763
Tobias Fischer <tobiasfischer17@gmail.com>
2024-10-18 18:28:48 -0500

    working restore

--- 13_token_cluster/train.py
+++ 13_token_cluster/train.py
@@ -14,8 +14,10 @@ from modelfile import create_models, MAX_CLUSTER_CONTEXT, CLUSTER_SIZE
 
 @dataclass
 class TrainingData:
-   train_losses: List[Dict[str,List[float]]]
+   train_losses: List[List[Dict[str,float]]]
    last_weight_files: List[str]
+   dataset_i: int = 0
+   step_i: int = 0
 
    @staticmethod
    def from_json(data:Dict) -> 'TrainingData': return TrainingData(**data)
@@ -23,6 +25,8 @@ class TrainingData:
 
 
 def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, predict_loss:bool, keep_all_weights:bool):
+   assert any([cluster_loss, decoded_loss, predict_loss]), "atleast 1 loss type is required, got 0"
+
    Tensor.manual_seed(42)
    Tensor.training = True
    BEAM_VALUE = BEAM.value
@@ -34,16 +38,17 @@ def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, pre
 
    # Potentially Pick Up Old Weights
    if restore is not None:
-      path = os.path.realpath(os.path.join(restore, "data.json"))
-      assert os.path.isfile(path), f"failed to find data json at the restore path, searched for {path}"
-      with open(path, "r") as f:
+      weights_folder = os.path.realpath(restore)
+      data_path = os.path.realpath(os.path.join(weights_folder, "data.json"))
+      assert os.path.isfile(data_path), f"failed to find data json at the restore path, searched for {data_path}"
+      with open(data_path, "r") as f:
          data = TrainingData.from_json(json.load(f))
-      for i, path in enumerate(data.last_weight_files):
-         assert os.path.exists(path), f"failed to find weights path for restore model, searched for {path}"
-         nn.state.load_state_dict(models[i], nn.state.safe_load(path))
+      for i, weight_path in enumerate(data.last_weight_files):
+         assert os.path.exists(weight_path), f"failed to find weights path for restore model, searched for {weight_path}"
+         nn.state.load_state_dict(models[i], nn.state.safe_load(weight_path))
    else:
       weights_folder = f"weights/{os.path.basename(os.path.dirname(__file__))}/{datetime.datetime.now()}".replace(" ", "_").replace(":", "_").replace("-", "_").replace(".", "_")
-      data = TrainingData([dict() for _ in range(MODEL_CONFIGS)], [])
+      data = TrainingData([[] for _ in range(MODEL_CONFIGS)], [])
 
    # Load Dataset
    X_train, X_val = [np.memmap(f"/raid/datasets/fineweb/tokenized/fineweb_{split}.bin", dtype=np.uint16, mode='r') for split in ('train', 'val')]
@@ -89,15 +94,19 @@ def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, pre
    GLOBAL_BS = DEVICE_BS * GPUS_PER_MODEL
    TOKENS_CONTEXT_SIZE = MAX_CLUSTER_CONTEXT * CLUSTER_SIZE
 
-   GRAPH_EVERY = 200
-   SAVE_EVERY  = 10000
+   AVERAGE_EVERY = 200
+   GRAPH_EVERY   = 200
+   SAVE_EVERY    = 10000
+
+   train_loss_chunks: List[Dict[str,List[float]]] = [{} for _ in range(MODEL_CONFIGS)]
+   average_index_start = data.dataset_i
 
    @TinyJit
    def train_step(orig_tokens:Tensor) -> List[Tuple[Tensor,Dict[str,Tensor],Tensor]]:
       ret_val = []
       for i in range(MODEL_CONFIGS):
          dev_tokens = orig_tokens.to(GPUS[i]) if GPUS_PER_MODEL == 1 else orig_tokens.shard(GPUS[i*GPUS_PER_MODEL:(i+1)*GPUS_PER_MODEL], axis=0)
-         losses, acc = models[i].compute_loss(dev_tokens)
+         losses, acc = models[i].compute_loss(dev_tokens, cluster_loss, decoded_loss, predict_loss)
          loss = sum(losses.values()).realize()
          optims[i].zero_grad()
          loss.backward()
@@ -106,40 +115,58 @@ def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, pre
          ret_val.append((loss, losses, acc))
       return ret_val
 
-   step_i = 0
-   dataset_i = 0
    with Tensor.train():
       while True:
          # Configure Step
          start_time = time.time()
-         Tensor.manual_seed(step_i)
+         Tensor.manual_seed(data.step_i)
 
          # Perform Training Step
          loss_vs, acc_vs = [], []
          with Context(BEAM=BEAM_VALUE):
-            orig_batches = [Tensor(np.asarray(X_train[dataset_i + batch_i*TOKENS_CONTEXT_SIZE :dataset_i + (batch_i+1)*TOKENS_CONTEXT_SIZE]), dtype=dtypes.int32) for batch_i in range(GLOBAL_BS)]
+            orig_batches = [Tensor(np.asarray(X_train[data.dataset_i + batch_i*TOKENS_CONTEXT_SIZE :data.dataset_i + (batch_i+1)*TOKENS_CONTEXT_SIZE]), dtype=dtypes.int32) for batch_i in range(GLOBAL_BS)]
             orig_tokens = Tensor.stack(*orig_batches)
             ret_vals = train_step(orig_tokens.realize())
 
             for i, (loss, losses, acc) in enumerate(ret_vals):
                loss_vs.append(loss.item())
                acc_vs.append(acc.item())
-               for k,v in losses.items():
-                  if k not in data.train_losses[i]:
-                     data.train_losses[i][k] = []
-                  data.train_losses[i][k].append(v.item())
+
+               for label, value in losses.items():
+                  if label not in train_loss_chunks[i]:
+                     train_loss_chunks[i][label] = []
+                  train_loss_chunks[i][label].append(value.item())
 
          # Increment Counters
-         step_i += 1
-         dataset_i += TOKENS_CONTEXT_SIZE * GLOBAL_BS
+         data.step_i += 1
+         data.dataset_i += TOKENS_CONTEXT_SIZE * GLOBAL_BS
+
+         # Average the Loss Data
+         if data.step_i > 0 and data.step_i % AVERAGE_EVERY == 0:
+            for i in range(MODEL_CONFIGS):
+               losses = { k:sum(v)/len(v) for k,v in train_loss_chunks[i].items() }
+               losses["index"] = average_index_start
+               data.train_losses[i].append(losses)
+            average_index_start = data.dataset_i
+            train_loss_chunks = [{} for _ in range(MODEL_CONFIGS)]
 
          # Create the Plots
-         if step_i > 0 and step_i % GRAPH_EVERY == 0:
+         if data.step_i > 0 and data.step_i % GRAPH_EVERY == 0:
             for i in range(MODEL_CONFIGS):
                plt.clf()
-               x = np.arange(step_i)
-               for label, y in data.train_losses[i].items():
-                  plt.plot(x, y, label=label)
+               xs: Dict[str,List[float]] = {}
+               ys: Dict[str,List[float]] = {}
+               for chunk in data.train_losses[i]:
+                  for label in chunk:
+                     if label == "index":
+                        continue
+                     if label not in xs:
+                        xs[label] = []
+                        ys[label] = []
+                     xs[label].append(chunk["index"])
+                     ys[label].append(chunk[label])
+               for label in xs.keys():
+                  plt.plot(xs[label], ys[label], label=label)
                plt.ylim((0,None))
                plt.title("Loss")
                plt.legend()
@@ -148,12 +175,12 @@ def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, pre
                if not os.path.exists(weights_folder): os.makedirs(weights_folder)
                plt.savefig(os.path.join(weights_folder, f"graph_loss_c{i}.png"), dpi=100)
          
-         if step_i > 0 and step_i % SAVE_EVERY == 0:
+         if data.step_i > 0 and data.step_i % SAVE_EVERY == 0:
             # Save All of the Models Weights
             new_weight_files = []
             if not os.path.exists(weights_folder): os.makedirs(weights_folder)
             for i in range(MODEL_CONFIGS):
-               new_weight_files.append(save_path := os.path.join(weights_folder, f"model_{step_i//1000:04d}k_c{i}.st"))
+               new_weight_files.append(save_path := os.path.join(weights_folder, f"model_{data.step_i//1000:04d}k_c{i}.st"))
                nn.state.safe_save(nn.state.get_state_dict(models[i]), save_path)
             
             # Potentially Purge the Last Weights Saved
@@ -166,10 +193,11 @@ def train_model(restore:Optional[str], cluster_loss:bool, decoded_loss:bool, pre
                      except Exception as ex: print(f"WARNING: ran into error deleting old weights file, {ex}")
                data.last_weight_files = new_weight_files
             
+            # Save a Data Json
             with open(os.path.join(weights_folder, f"data.json"), "w") as f:
                json.dump(data.to_json(), f)
 
-         print(f"| {step_i-1:05d} | {1000.0*(time.time()-start_time):.0f} ms | Train Loss " + " - ".join(f"{l:.4f}" for l in loss_vs) + f" | Train Acc " + " - ".join(f"{100.0*a:.2f}%" for a in acc_vs) + " |")
+         print(f"| {data.step_i-1:05d} | {1000.0*(time.time()-start_time):.0f} ms | Train Loss " + " - ".join(f"{l:.4f}" for l in loss_vs) + f" | Train Acc " + " - ".join(f"{100.0*a:.2f}%" for a in acc_vs) + " |")
 
 if __name__ == "__main__":
    import argparse
@@ -180,4 +208,4 @@ if __name__ == "__main__":
    parser.add_argument('--predict-loss', action='store_true')
    parser.add_argument('--keep-all-weights', action='store_true')
    args = parser.parse_args()
-   train_model(args.restore, args.cluster_loss, args.decoded_loss, args.predict_loss, args.keep_all_weights)
+   train_model(args.restore_folder, args.cluster_loss, args.decoded_loss, args.predict_loss, args.keep_all_weights)
