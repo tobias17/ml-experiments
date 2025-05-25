bff3504058f7a5621c60317b69b556e06578698a
Tobias Fischer <tobiasfischer17@gmail.com>
2025-05-25 12:03:33 -0500

    working iteration and saving

--- 15_cheat_sheet/splice.py
+++ 15_cheat_sheet/splice.py
@@ -1,34 +1,30 @@
-from tinygrad import Tensor, dtypes, nn, Device, TinyJit
-from typing import List, Tuple
-import numpy as np
+from tinygrad import Tensor, dtypes, nn, Device, TinyJit, Context
+from tqdm import tqdm
 import os
 
 from common import (
-   load_tokenizer, make_filename, cosine_similarity,
+   make_filename, cosine_similarity,
    ENTRIES_PER_FILE,
 )
 
-LOAD_SLICES: int = 1024 * 4
-LOAD_SIZE:   int = ENTRIES_PER_FILE // LOAD_SLICES
+LOAD_SIZE:   int = 64
+LOAD_SLICES: int = ENTRIES_PER_FILE // LOAD_SIZE
 assert LOAD_SLICES * LOAD_SIZE == ENTRIES_PER_FILE
 
 WIKI_ROOT     = "/raid/datasets/wikipedia/sentence-embedding"
 FINE_WEB_ROOT = "/raid/datasets/fineweb/sentence-embedding"
+OUT_ROOT      = "/raid/datasets/fineweb/with-wiki-top1"
+if not os.path.exists(OUT_ROOT):
+   os.makedirs(OUT_ROOT)
 
-EMB_GPUS = tuple(f"{Device.DEFAULT}:{i}" for i in range(1,5))
-# TOK_GPUS = tuple(f"{Device.DEFAULT}:{i}" for i in range(4,6))
+GPUS = tuple(f"{Device.DEFAULT}:{i}" for i in range(1,5))
+EMB_DTYPE = dtypes.float16
 
 def main():
-   tokenizer = load_tokenizer()
-
    ######################
    #   Load Wiki Data   #
    ######################
    wiki_list = {} # type: ignore
-   dtype_dev_map = {
-      "emb": (dtypes.float16, EMB_GPUS),
-      # "tok": (dtypes.int32,   TOK_GPUS),
-   }
    tok_disktensors = []
    for i in range(1024):
       filepath = os.path.join(WIKI_ROOT, make_filename(i))
@@ -38,13 +34,11 @@ def main():
       for k, v in sd.items():
          if k == "tok":
             tok_disktensors.append(v)
+         if k != "emb":
             continue
-         if k not in dtype_dev_map:
-            continue
-         target_dtype, gpus = dtype_dev_map[k]
          if k not in wiki_list:
             wiki_list[k] = []
-         new_value = v.reshape(-1, len(gpus), v.shape[-1]).shard(gpus, axis=1).cast(target_dtype).realize()
+         new_value = v.reshape(-1, len(GPUS), v.shape[-1]).shard(GPUS, axis=1).cast(EMB_DTYPE).realize()
          wiki_list[k].append(new_value)
    wiki = {}
    for k, v in wiki_list.items():
@@ -52,23 +46,17 @@ def main():
       print(f"{k}: {wiki[k].shape}")
 
    wiki_emb: Tensor = wiki["emb"]
-   # wiki_tok: Tensor = wiki["tok"]
 
    @TinyJit
-   def _get_closest_wiki_entry_index(emb:Tensor) -> Tuple[Tensor,Tensor]:
-      sim = cosine_similarity(emb.unsqueeze(1).unsqueeze(1).shard(EMB_GPUS), wiki_emb.unsqueeze(0)).to(Device.DEFAULT).realize()
+   def _get_closest_wiki_entry_index(emb:Tensor) -> Tensor:
+      sim = cosine_similarity(emb.cast(EMB_DTYPE).unsqueeze(1).unsqueeze(1).shard(GPUS), wiki_emb.unsqueeze(0)).to(Device.DEFAULT).realize()
       sim = sim.to_(Device.DEFAULT).realize()
-      sim = sim.reshape(emb.shape[0], -1)
-      print(sim.shape)
-      argmax = sim.argmax(axis=-1)
-      print(argmax.shape)
-      return argmax.realize(), sim[:,argmax].mul(Tensor.eye(argmax.shape[0])).sum(axis=-1).realize()
+      return sim.reshape(emb.shape[0], -1).argmax(axis=-1).realize()
 
    def get_closest_wiki_entry(emb:Tensor) -> Tensor:
-      index, sims = _get_closest_wiki_entry_index(emb.realize())
-      index = index.numpy()
-      print(index)
-      print(sims.numpy())
+      with Context(BEAM=1):
+         index = _get_closest_wiki_entry_index(emb.realize()).numpy()
+      index = index
       toks = []
       for i in index:
          i = int(i)
@@ -78,21 +66,27 @@ def main():
    #####################
    #   Load Fine Web   #
    #####################
-   for i in range(1,1024):
-      filepath = os.path.join(FINE_WEB_ROOT, make_filename(i))
-      if not os.path.exists(filepath):
+   for i in range(1024):
+      filename = make_filename(i)
+      print(filename)
+      output_filepath = os.path.join(OUT_ROOT, filename)
+      if os.path.exists(output_filepath):
+         continue
+      input_filepath = os.path.join(FINE_WEB_ROOT, filename)
+      if not os.path.exists(input_filepath):
          break
-      sd = nn.state.safe_load(filepath)
+      sd = nn.state.safe_load(input_filepath)
       fine_web_emb = sd["emb"].to(Device.DEFAULT).realize()
-      fine_web_tok = sd["tok"].to(Device.DEFAULT).numpy()
-      for k in range(LOAD_SLICES):
-         wiki_tokens = get_closest_wiki_entry(fine_web_emb[k*LOAD_SIZE:(k+1)*LOAD_SIZE].contiguous().realize())
-         fine_web_tokens = fine_web_tok[k*LOAD_SIZE:(k+1)*LOAD_SIZE]
-         wiki_text     = tokenizer.Decode(wiki_tokens.numpy().tolist())
-         fine_web_text = tokenizer.Decode(fine_web_tokens.tolist())
-         for wiki_row, fine_web_row in zip(wiki_text, fine_web_text):
-            print("\n"+">"*40+" fine web\n"+fine_web_row+"\n"+"="*40+" wiki\n"+wiki_row+"\n"+"<"*40+"\n")
-         return
+
+      wiki_tokens = []
+      for k in tqdm(range(LOAD_SLICES), disable=False):
+         wiki_tokens.append(get_closest_wiki_entry(fine_web_emb[k*LOAD_SIZE:(k+1)*LOAD_SIZE].contiguous().realize()))
+      data = {
+         "wiki": Tensor.cat(*wiki_tokens),
+         "tok":  sd["tok"],
+      }
+      nn.state.safe_save(data, output_filepath)
+      print(f"Saved data wiki[{data['wiki'].shape}] tok[{data['tok'].shape}]")
 
 
 if __name__ == "__main__":
