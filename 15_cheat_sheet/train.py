26c92f78da26e42b628a0072c635607f3a4fadd1
Tobias Fischer <tobiasfischer17@gmail.com>
2025-07-06 15:21:16 -0500

    eval gen code

--- 15_cheat_sheet/train.py
+++ 15_cheat_sheet/train.py
@@ -8,12 +8,15 @@ import numpy as np
 
 from util import compress, fmt_digits, fmt_percent, fmt_time
 from model import ModelConfig, Model
-from common import make_filename, loglerp, ENTRIES_PER_FILE
+from common import make_filename, loglerp, load_tokenizer, ENTRIES_PER_FILE
 
 
 BASELINE_SMALL = ModelConfig(cross_attn=False, n_layers=16)
 CHEAT_SHEET    = ModelConfig(cross_attn=True,  n_layers=16)
 BASELINE_LARGE = ModelConfig(cross_attn=False, n_layers=24, ff_mult=6.0)
+
+CHEAT_SHEET    = ModelConfig(cross_attn=True,  n_layers=4, ff_mult=2.0)
+BASELINE_LARGE = ModelConfig(cross_attn=False, n_layers=6, ff_mult=3.0)
 
 
 @dataclass
@@ -21,6 +24,7 @@ class TrainingData:
    train_losses: Dict[str,List[float]]
    train_accs: Dict[str,List[float]]
    last_weight_files: Dict[str,str]
+   eval_data: List[Dict[str,List[Dict[str,str]]]]
    dataset_i: int = 0
    step_i: int = 0
 
@@ -30,7 +34,6 @@ class TrainingData:
 
 def get_models(print_params:bool=True) -> Dict[str,Model]:
    models = {
-      "baseline_small": Model(BASELINE_SMALL),
       "cheat_sheet":    Model(CHEAT_SHEET),
       "baseline_large": Model(BASELINE_LARGE),
    }
@@ -67,23 +70,34 @@ class DataLoader:
       return process("tok"), process("wiki")
 
 
-BS = 4
+BS   = 1
 LR_A = 2**-14
 LR_B = 2**-17
 
 AVERAGE_EVERY = 500
 GRAPH_EVERY   = 500
+TEST_EVERY    = 10
+EVAL_EVERY    = 1
 SAVE_EVERY    = 5000
 
 MAX_DATASET_ENTRIES = 8_900_000
+EVAL_GEN_AMOUNT     = 32
 
 
 def train(restore:str|None=None, keep_all_weights:bool=False):
    Tensor.no_grad = False
 
    data_loader = DataLoader()
-   print(f"\nMax dataset index: {data_loader.max_index}\nMax dataset entry: {MAX_DATASET_ENTRIES}")
+   print(f"\nMax dataset size: {data_loader.max_index}\nMax dataset usage: {MAX_DATASET_ENTRIES}")
+
+   with open(os.path.join(DATASET_ROOT, "eval.json")) as f:
+      eval_text = json.load(f)
+   eval_data = nn.state.safe_load(os.path.join(DATASET_ROOT, "eval.st"))
+   for w in eval_data.values(): w.to_(Device.DEFAULT).realize()
+   print(f"Eval entries: {eval_data['tok'].shape[0]}")
+
    models = get_models(print_params=True)
+   tokenizer = load_tokenizer()
 
    if restore is not None:
       weights_folder = os.path.realpath(restore)
@@ -97,14 +111,14 @@ def train(restore:str|None=None, keep_all_weights:bool=False):
          nn.state.load_state_dict(models[name], nn.state.safe_load(weight_path))
    else:
       weights_folder = f"weights/{os.path.basename(os.path.dirname(__file__))}/{datetime.datetime.now()}".replace(" ", "_").replace(":", "_").replace("-", "_").replace(".", "_")
-      data = TrainingData({k:[] for k in models.keys()}, {k:[] for k in models.keys()}, {})
+      data = TrainingData({k:[] for k in models.keys()}, {k:[] for k in models.keys()}, {}, [])
 
    optims: Dict[str,nn.optim.LAMB] = {}
    for i, (name, model) in enumerate(models.items()):
       params = nn.state.get_parameters(model)
       for p in params:
-         p.shard_((f"{Device.DEFAULT}:{i*2}", f"{Device.DEFAULT}:{i*2+1}"))
-         # p.to_(f"{Device.DEFAULT}:{i+2}")
+         # p.shard_((f"{Device.DEFAULT}:{i*2}", f"{Device.DEFAULT}:{i*2+1}"))
+         p.to_(f"{Device.DEFAULT}:{i}")
       optims[name] = nn.optim.AdamW(params, lr=LR_A, eps=1e-5)
 
    @TinyJit
@@ -122,95 +136,141 @@ def train(restore:str|None=None, keep_all_weights:bool=False):
          accs[k] = Tensor.cast(logits.argmax(-1) == y, dtypes.float32).mean().realize()
       return losses, accs
 
+   @TinyJit
+   def test_step(tok:Tensor, ctx:Tensor) -> Tuple[Dict[str,Tensor],Dict[str,Tensor]]:
+      losses: Dict[str,Tensor] = {}
+      accs:   Dict[str,Tensor] = {}
+      for k, model in models.items():
+         dev_tok = tok.to(model.device) if isinstance(model.device, str) else tok.shard(model.device, axis=0)
+         dev_ctx = ctx.to(model.device) if isinstance(model.device, str) else ctx.shard(model.device, axis=0)
+         logits = model(dev_tok[:,:-1], dev_ctx if model.cross_attn else None)
+         y = dev_tok[:,1:]
+         losses[k] = logits.sparse_categorical_crossentropy(y)
+         accs[k] = Tensor.cast(logits.argmax(-1) == y, dtypes.float32).mean().realize()
+      return losses, accs
+
+   def eval_step(model:Model, tok:Tensor, ctx:Tensor) -> Tensor:
+      dev_tok = tok.to(model.device) if isinstance(model.device, str) else tok.shard(model.device, axis=0)
+      dev_ctx = ctx.to(model.device) if isinstance(model.device, str) else ctx.shard(model.device, axis=0)
+      return model(dev_tok[:,:-1], dev_ctx if model.cross_attn else None).realize()
+   eval_steps = { k: TinyJit(eval_step) for k in models.keys() }
+   def run_evals() -> Dict[str,List[Dict[str,str]]]:
+      gens: Dict[str,List[Dict[str,str]]] = {}
+      for k, model in models.items():
+         gens[k] = []
+         for i in range(int(eval_data["tok"].shape[0])):
+            wiki = eval_data["wiki"][i:i+1].contiguous().realize()
+            tok  = eval_data["tok" ][i:i+1].contiguous().realize()
+            size = eval_data["size"][i].item()
+            acc  = []
+            tok.requires_grad = False
+            for _ in range(EVAL_GEN_AMOUNT):
+               logits = eval_steps[k](model, tok, wiki)
+               new_tok = logits[0, size-1].argmax().item()
+               acc.append(new_tok)
+               tok[0, size] = new_tok
+               tok = tok.contiguous().realize()
+               size += 1
+            gens[k].append({
+               "q": eval_text[i]["question"],
+               "a": tokenizer.Decode(acc),
+            })
+      return gens
+
    loss_vs: List[Dict[str,float]] = []
    acc_vs:  List[Dict[str,float]] = []
-   with Tensor.train():
-      while True:
-         # Configure Step
-         start_time = time.perf_counter()
-         Tensor.manual_seed(data.step_i)
+   while True:
+      # Configure Step
+      start_time = time.perf_counter()
+      Tensor.manual_seed(data.step_i)
 
-         # Overrun check
-         if data.dataset_i >= MAX_DATASET_ENTRIES:
-            print(f"Trained on {data.dataset_i} tokens")
+      # Overrun check
+      if data.dataset_i >= MAX_DATASET_ENTRIES:
+         print(f"Trained on {data.dataset_i} tokens")
 
-         # Set the optim LR for decaying items
-         new_lr = loglerp(LR_A, LR_B, data.dataset_i / MAX_DATASET_ENTRIES)
-         for o in optims.values():
-            o.lr.assign(Tensor([new_lr], device=o.lr.device))
+      # Set the optim LR for decaying items
+      new_lr = loglerp(LR_A, LR_B, data.dataset_i / MAX_DATASET_ENTRIES)
+      for o in optims.values():
+         o.lr.assign(Tensor([new_lr], device=o.lr.device))
 
-         # Perform Training Step
+      # Perform Training Step
+      with Tensor.train():
          tok, ctx = data_loader.get(data.dataset_i, BS)
          loss, acc = train_step(tok.realize(), ctx.realize())
          loss_vs.append({ k:l.item() for k,l in loss.items()})
          acc_vs .append({ k:a.item() for k,a in acc .items()})
 
-         # Increment Counters
-         data.step_i += 1
-         data.dataset_i += BS
-         print(" | ".join([
-            f"{data.step_i: 9d}",
-            "Dataset: " + fmt_percent(data.dataset_i/MAX_DATASET_ENTRIES),
-            fmt_time(time.perf_counter()-start_time,4),
-            "Train Loss: " + " - ".join(fmt_digits(l,5) for l in loss_vs[-1].values()),
-            "Train Acc: "  + " - ".join(fmt_percent(a)  for a in acc_vs [-1].values()),
-            f"LR: {math.log2(new_lr):.3f}",
-         ]))
+      # Increment Counters
+      data.step_i += 1
+      data.dataset_i += BS
+      print(" | ".join([
+         f"{data.step_i: 9d}",
+         "Dataset: " + fmt_percent(data.dataset_i/MAX_DATASET_ENTRIES),
+         fmt_time(time.perf_counter()-start_time,4),
+         "Train Loss: " + " - ".join(fmt_digits(l,5) for l in loss_vs[-1].values()),
+         "Train Acc: "  + " - ".join(fmt_percent(a)  for a in acc_vs [-1].values()),
+         f"LR: {math.log2(new_lr):.3f}",
+      ]))
 
-         # Average the Loss Data
-         if data.step_i > 0 and data.step_i % AVERAGE_EVERY == 0:
-            for k in models.keys():
-               data.train_losses[k].append(sum(l[k] for l in loss_vs)/len(loss_vs))
-               data.train_accs  [k].append(sum(a[k] for a in acc_vs )/len(acc_vs ))
+      if data.step_i > 0 and data.step_i % EVAL_EVERY == 0:
+         with Tensor.test():
+            gens = run_evals()
+            data.eval_data.append(gens)
 
-         # Create the Plots
-         if data.step_i > 0 and data.step_i % GRAPH_EVERY == 0:
-            def plot(items, title:str, ymax:float|None=None):
-               xs, ys = {}, {}
-               for name in models.keys():
-                  xs[name] = (np.arange(len(items[name]))+1)*AVERAGE_EVERY*BS
-                  ys[name] = np.array(items[name])
-               plt.clf()
-               max_95th = 0.0
-               for label in xs.keys():
-                  if ymax is None:
-                     max_95th = max(max_95th, np.percentile(ys[label], 95))
-                  plt.plot(xs[label], ys[label], label=label)
-               plt.ylim((0, max_95th*1.2 if ymax is None else ymax))
-               plt.xlim((0,None))
-               plt.title(title)
-               plt.legend()
-               figure = plt.gcf()
-               figure.set_size_inches(18, 10)
-               if not os.path.exists(weights_folder): os.makedirs(weights_folder)
-               plt.savefig(os.path.join(weights_folder, f"graph_{title.lower()}.png"), dpi=100)
-            plot(data.train_losses, "Loss")
-            plot(data.train_accs,   "Acc", ymax=1.0)
+      # Average the Loss Data
+      if data.step_i > 0 and data.step_i % AVERAGE_EVERY == 0:
+         for k in models.keys():
+            data.train_losses[k].append(sum(l[k] for l in loss_vs)/len(loss_vs))
+            data.train_accs  [k].append(sum(a[k] for a in acc_vs )/len(acc_vs ))
 
-         if data.step_i > 0 and data.step_i % SAVE_EVERY == 0:
-            # Save All of the Models Weights
-            new_weight_files = {}
-            if not os.path.exists(weights_folder):
-               os.makedirs(weights_folder)
-            for key in models.keys():
-               new_weight_files[key] = f"model_{data.step_i//1000:04d}k_{key}.st"
-               nn.state.safe_save(nn.state.get_state_dict(models[key]), os.path.join(weights_folder, new_weight_files[key]))
+      # Create the Plots
+      if data.step_i > 0 and data.step_i % GRAPH_EVERY == 0:
+         def plot(items, title:str, ymax:float|None=None):
+            xs, ys = {}, {}
+            for name in models.keys():
+               xs[name] = (np.arange(len(items[name]))+1)*AVERAGE_EVERY*BS
+               ys[name] = np.array(items[name])
+            plt.clf()
+            max_95th = 0.0
+            for label in xs.keys():
+               if ymax is None:
+                  max_95th = max(max_95th, np.percentile(ys[label], 95))
+               plt.plot(xs[label], ys[label], label=label)
+            plt.ylim((0, max_95th*1.2 if ymax is None else ymax))
+            plt.xlim((0,None))
+            plt.title(title)
+            plt.legend()
+            figure = plt.gcf()
+            figure.set_size_inches(18, 10)
+            if not os.path.exists(weights_folder): os.makedirs(weights_folder)
+            plt.savefig(os.path.join(weights_folder, f"graph_{title.lower()}.png"), dpi=100)
+         plot(data.train_losses, "Loss")
+         plot(data.train_accs,   "Acc", ymax=1.0)
 
-            # Potentially Purge the Last Weights Saved
-            if not keep_all_weights:
-               for filename in data.last_weight_files.values():
-                  path = os.path.join(weights_folder, filename)
-                  if path in new_weight_files.values():
-                     print("WARNING: weights overwrote themselves, skipping purge step")
-                  else:
-                     print(f"Saving {path}")
-                     try: os.remove(path)
-                     except Exception as ex: print(f"WARNING: ran into error deleting old weights file, {ex}")
-            data.last_weight_files = new_weight_files
+      if data.step_i > 0 and data.step_i % SAVE_EVERY == 0:
+         # Save All of the Models Weights
+         new_weight_files = {}
+         if not os.path.exists(weights_folder):
+            os.makedirs(weights_folder)
+         for key in models.keys():
+            new_weight_files[key] = f"model_{data.step_i//1000:04d}k_{key}.st"
+            nn.state.safe_save(nn.state.get_state_dict(models[key]), os.path.join(weights_folder, new_weight_files[key]))
 
-            # Save a Data Json
-            with open(os.path.join(weights_folder, f"data.json"), "w") as f:
-               json.dump(data.to_json(), f)
+         # Potentially Purge the Last Weights Saved
+         if not keep_all_weights:
+            for filename in data.last_weight_files.values():
+               path = os.path.join(weights_folder, filename)
+               if path in new_weight_files.values():
+                  print("WARNING: weights overwrote themselves, skipping purge step")
+               else:
+                  print(f"Saving {path}")
+                  try: os.remove(path)
+                  except Exception as ex: print(f"WARNING: ran into error deleting old weights file, {ex}")
+         data.last_weight_files = new_weight_files
+
+         # Save a Data Json
+         with open(os.path.join(weights_folder, f"data.json"), "w") as f:
+            json.dump(data.to_json(), f)
 
 
 
